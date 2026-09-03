8e7fe722bca9e7ee8cbb0d134f868c7fd1bf8cde
RedDay1312 <xxmonkexx13@gmail.com>
2026-09-03 19:58:01 +0300

    Update PWA cache for history feature and cleanup old cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='krestiki-arena-v1';
-const ASSETS=['./','./index.html','./style.css','./script.js','./enhancements.js','./manifest.webmanifest'];
+const CACHE='krestiki-arena-v2';
+const ASSETS=['./','./index.html','./style.css','./extras.css','./script.js','./enhancements.js','./extras.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
